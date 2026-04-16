9c445598a0bd2890e3d5fa6f0404042863f1957b
MatiasMC1 <matis123457@hotmail.com>
2026-04-16 09:28:54 +0200

    Health script done

---
+++ Backyard RC wars/Assets/Scripts/Health.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public int tankHealth;
+    int currentHealth;
+
+    private void Awake()
+    {
+        currentHealth = tankHealth;
+    }
+
+    private void Update()
+    {
+        Death();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    void Death()
+    {
+        if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene("WinScene");
+        }
+    }
+}
