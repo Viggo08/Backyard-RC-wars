07c54c97107506f7536264a08dce75e0e71709ec
Adam Pantzar <adam.pantzar@gmail.com>
2026-05-21 09:18:20 +0200

    Made camera find players upon spawn

--- Backyard RC wars/Assets/AdamFolder/Scripts/CameraController.cs
+++ Backyard RC wars/Assets/AdamFolder/Scripts/CameraController.cs
@@ -1,12 +1,9 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CameraController : MonoBehaviour
 {
-    [Header("Targets")]
-    [SerializeField] GameObject _target0;
-    [SerializeField] GameObject _target1;
-
     [Header("Settings")]
     [SerializeField] float _easingSpeed = 10f;
     [SerializeField] float _baseFOV = 20f;
@@ -16,6 +13,8 @@ public class CameraController : MonoBehaviour
 
     [Header("Screen Shake")]
     [SerializeField] float _shakeBaseDuration = 0.3f;
+
+    public List<GameObject> Targets = new List<GameObject>(); //Make this work somehow
 
     Camera _camera;
     Vector3 _shakeOffset;
@@ -32,13 +31,16 @@ public class CameraController : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(Targets.Count);
+        if (Targets.Count < 2) return;
+
         FollowMidpoint();
         AdjustFOV();
     }
 
     void FollowMidpoint()
     {
-        Vector3 midpoint = (_target0.transform.position + _target1.transform.position) / 2f;
+        Vector3 midpoint = (Targets[0].transform.position + Targets[1].transform.position) / 2f; //Adjust for list
         Vector3 targetPosition = new Vector3(midpoint.x, transform.position.y, midpoint.z);
 
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * _easingSpeed) + _shakeOffset;
@@ -46,7 +48,7 @@ public class CameraController : MonoBehaviour
 
     void AdjustFOV()
     {
-        float distance = Vector3.Distance(_target0.transform.position, _target1.transform.position);
+        float distance = Vector3.Distance(Targets[0].transform.position, Targets[1].transform.position); //Adjust for list
         float targetFOV = Mathf.Clamp(_baseFOV + distance * _fovPerUnit, _minFOV, _maxFOV);
 
         _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, targetFOV, Time.deltaTime * _easingSpeed);
